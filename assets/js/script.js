5ca752faedfde7998cd32faa6dedec94f794f28a
Rebeljoka <habib.u99@googlemail.com>
2025-08-10 17:36:43 +0100

    Add: display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
  * the main game "loop", called when the script is first loaded
     * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // Game logic goes here
 
     // Generate two random numbers between 1 and 25
     // These numbers will be used for the math questions
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -47,16 +56,28 @@ function incrementWrongAnswer() {
 
 function displayAdditionQuestion(operand1, operand2) {
     // Display addition question logic
+    document.getElementById("opperand1").textContent = operand1;
+    document.getElementById("opperand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion(operand1, operand2) {
     // Display subtraction question logic
-} 
+    document.getElementById("opperand1").textContent = operand1;
+    document.getElementById("opperand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
     // Display multiplication question logic
+    document.getElementById("opperand1").textContent = operand1;
+    document.getElementById("opperand2").textContent = operand2;
+    document.getElementById("operator").textContent = "*";
 }
 
 function displayDivisionQuestion(operand1, operand2) {
     // Display division question logic
+    document.getElementById("opperand1").textContent = operand1;
+    document.getElementById("opperand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }  
